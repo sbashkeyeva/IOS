1e15cca0ea501a865850e6970d9b1e19b771d7d3
Symbat <sbashkeyeva@gmail.com>
2019-02-20 20:56:42 +0600

    smth's going to happen

---
+++ week2/QuizApp/QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Symbat Bashkeyeva on 2/20/19.
+//  Copyright © 2019 Symbat Bashkeyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
