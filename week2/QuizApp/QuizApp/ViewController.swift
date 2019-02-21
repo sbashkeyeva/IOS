acbcf17d399759cc374709489997188891104fdc
Symbat <sbashkeyeva@gmail.com>
2019-02-21 16:47:41 +0600

    added method checkAnwer

--- week2/QuizApp/QuizApp/ViewController.swift
+++ week2/QuizApp/QuizApp/ViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let allQuestions=QuestionBank()
+    var pickedAnswer:Bool=false
+    var questionNumber:Int=0
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
-    @IBAction func answerPressed(_ sender: Any) {
+    @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag==1 {
+            pickedAnswer=true
+        }
+        else if sender.tag==2{
+            pickedAnswer=false
+        }
+        checkAnswer()
+        questionNumber+=1
+        questionLabel.text=allQuestions.list[questionNumber].questionText
+    }
+    
+    func checkAnswer() {
+        let correctAnswer=allQuestions.list[questionNumber].answer
+        if correctAnswer==pickedAnswer{
+            print("You got it")
+        }
+        else{
+            print("Wrong")
+        }
+        
+        
     }
 
 }
